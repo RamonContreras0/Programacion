b3eb127d9ad5d089392caa84cdcfbc589038750f
RamonContreras0 <167796475+RamonContreras0@users.noreply.github.com>
2024-05-29 21:47:38 -0400

    hola mundo

---
+++ apuntes.py
@@ -0,0 +1,15 @@
+# Este es un Hola Mundo
+print("Hola Mundo")
+
+"""Este es un
+comentario multilinea"""
+
+name = "Ramon"
+print("Hola mi nombre es", name)
+
+import math
+estatura = 1.70
+peso = 70
+
+imc =peso/(estatura ** 2)
+print(math.trunc(imc))
