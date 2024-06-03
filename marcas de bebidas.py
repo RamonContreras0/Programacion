b3b8aa33cf1b21ce1fc248894146ccbd6c1b4589
RamonContreras0 <167796475+RamonContreras0@users.noreply.github.com>
2024-06-03 12:15:34 -0400

    Update marcas de bebidas.py

--- marcas de bebidas.py
+++ marcas de bebidas.py
@@ -3,11 +3,11 @@ a. Ingresar 4 nombres de marcas de bebida por consola y almacenarlos en una list
 b. Imprimir la lista completa.
 c. Mostrar el primer y último nombre de las bebidas de la lista."""
 
-marca1 = input("Ingrese la primera marca de bebidas")
-marca2 = input("Ingrese la segunda marca de bebidas")
-marca3 = input("Ingrese la tercera marca de bebidas")
-marca4 = input("Ingrese la cuarta marca de bebidas")
+marca1 = input("Ingrese la primera marca de bebidas ")
+marca2 = input("Ingrese la segunda marca de bebidas ")
+marca3 = input("Ingrese la tercera marca de bebidas ")
+marca4 = input("Ingrese la cuarta marca de bebidas ")
 
-marcas = marca1, marca2, marca3, marca4,
+marcas = [marca1, marca2, marca3, marca4]
 print("Las cuatro marcas son :", marcas)
 print("La primera y utima marca son: ", marcas[0], marcas[3])
