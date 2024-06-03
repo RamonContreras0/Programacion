60cbd3c1a0dd316a7ee29a85132175d4fb14f79d
RamonContreras0 <167796475+RamonContreras0@users.noreply.github.com>
2024-06-03 12:14:45 -0400

    Update apuntes.py

--- apuntes.py
+++ apuntes.py
@@ -30,8 +30,8 @@ musica = tuple()
 generos = ("Rock", "Blues", "Pop", "Trap", "HipHop")
 print(generos)
 print(type(generos))
-print(generos.index("Pop"))
-
+print(generos.index("Pop"))#Consulta la posicion de un elemeto
+#Cambio de estructura(Tupla a Lista)
 tuplita = ("Victor", 200, "Universidad", True)
 print(tuplita)
 tuplita = list(tuplita)
@@ -40,24 +40,27 @@ print("La tupla ahora es de tipo", type(tuplita))
 print(tuplita[0:3])
 
 #6. SETS (conjuntos)
+
 conjunto_vacio = set() # inicializar un set
 conjunto_x = {"JavaScript"} #las llaves sirven para set y dict
 print(type(conjunto_vacio))
 print(type(conjunto_x))
+
 #inicialisando Set
 colores = {"Azul", "Rojo", "Amarillo", "Verde", "Violeta"}
 animales = set({"Gato", "Perro", "Caballo", "Hamster"})
 print(colores)
 print(animales)
+
 #Agregando un nuevo elemento al set
 colores.add("Celeste")
 print(colores)
-#7. Diccionarios (Conjuntos)
 
+#7. Diccionarios (Conjuntos)
 paciente = dict(
     nombre = "Francisco",
-    edad = "30")
-
+    edad = "30",
+    cuidad = "Castro")
 hospital = {"Nombre" : "Augusto Riffart"}
 
 doctor = {
