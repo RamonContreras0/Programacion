6f8f8cd0e0ad0157d9f3699fe6f92269d714a99a
Ramon <ramonernesto.contreras@alumnos.ulagos.com>
2024-06-21 23:26:08 -0400

    terminado

--- Guias/#Guia 4 Ejercicio 2.py
+++ Guias/#Guia 4 Ejercicio 2.py
@@ -1,1 +1,18 @@
 #Guia 4 Ejercicio 2
+#Construir un programa permita calcular e imprimir el resultado de la siguiente sumatoria:
+#S = 500 + 456 + 510 + 454 + 520 + 452 + ... + 800 
+
+def resultado():
+ 
+    suma = 0
+    a = 500
+    b = 456
+
+    while a <= 800:
+        suma += a
+        suma += b
+        a += 10
+        b -= 2
+    return suma
+n = resultado()
+print(f"El resultado es: {n}")
