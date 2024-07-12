d41412b17140b9a1b4a3f2fd0b9f4190f3e403e3
Ramon <ramonernesto.contreras@alumnos.ulagos.com>
2024-07-12 09:30:06 -0400

    Parcial

--- Materia/factory.py
+++ Materia/factory.py
@@ -6,3 +6,27 @@ def factorial(num):
     return num
 
 print(factorial(5))
+
+def saludar(nombre):
+    print(f"Hola, {nombre}!")
+
+saludar("Juan")  # Aquí "Juan" es un argumento
+
+def sumar(a, b):
+    return a + b
+
+resultado = sumar(3, 5)
+print(resultado)  # Imprimirá 8
+
+def multiplicar(x, y, z):
+    return x * y * z
+
+resultado = multiplicar(2, 3, 4)
+print(resultado)  # Imprimirá 24
+
+def calcular_area_circulo(radio, pi=3.14):
+    return pi * radio * radio
+
+area = calcular_area_circulo(5)
+print(f"El área del círculo es: {area}")
+
